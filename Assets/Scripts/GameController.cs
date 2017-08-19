96d3f94422cf9659bb1707e4801c106ca6db8d77
Mann2019 <manpreetbawa04@gmail.com>
2017-08-19 11:33:17 +0530

    Script Changes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public float invokeTime;
+    public float invokeRate;
+    public GameObject roadPrefab;
+    public GameObject roadLead;
+    public GameObject car;
+
+    private Vector3 v;
+    private GameObject g;
+    private float roadSpeed;
+    private PlayerMover pm;
+
+    void Start () {
+        pm = car.GetComponent<PlayerMover>();
+        InvokeRepeating("GenerateRoad", invokeTime, invokeRate);
+	}
+
+    private void Update()
+    {
+        roadSpeed = pm.speed * pm.resultantFuel * -100;
+    }
+
+    public void GenerateRoad()
+    {
+        v = roadLead.transform.position;
+        g = Instantiate(roadPrefab, v, Quaternion.identity) as GameObject;
+        roadLead.transform.Translate(Vector3.forward * Time.deltaTime * roadSpeed, Space.Self);
+    }
+}
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+    public float destructionTimer;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("DestroyYourself");
+	}
+
+    IEnumerator DestroyYourself()
+    {
+        yield return new WaitForSeconds(destructionTimer);
+        Destroy(gameObject);
+    }
+}
